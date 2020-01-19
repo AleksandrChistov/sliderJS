0966f3c4987ee8c405e512fdc95b6a5d22c1048b
AleksandrChistov <aleksandrchistov.ru@yandex.ru>
2020-01-19 18:24:45 +0700

    Сделал добавление обертки для элементов слайдера / добавление своих стилей элементам слайдера

--- slider/slider.js
+++ slider/slider.js
@@ -1,5 +1,15 @@
 function sliderJS(selector, options) {
   const slider = document.querySelector(selector);
-  
+  const slides = document.querySelectorAll(selector + ' div');
+  const wrapper = document.createElement('div');
+    
   slider.classList.add('slider-js');
+  wrapper.classList.add('slider-js-wrap');
+
+  slides.forEach((slide, index) => {
+    slide.classList.add('slider-js__item');
+    wrapper.appendChild(slide);
+  });
+
+  slider.appendChild(wrapper);
 }
