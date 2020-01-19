61f9798856b72dcdbacfc9bfad38f8705e0f9957
AleksandrChistov <aleksandrchistov.ru@yandex.ru>
2020-01-19 19:21:20 +0700

    Создал функции: инициализации слайдера, добавления классов и элементов на страницу

--- slider/slider.js
+++ slider/slider.js
@@ -5,20 +5,31 @@ function sliderJS(selector, options) {
   const list = document.createElement('div');
   const arrowLeft = document.createElement('button');
   const arrowRight = document.createElement('button');
-    
-  slider.classList.add('slider-js');
-  wrapper.classList.add('slider-js-wrap');
-  list.classList.add('slider-js-list');
-  arrowLeft.classList.add('slider-js__arrow', 'slider-js__arrow-left');
-  arrowRight.classList.add('slider-js__arrow', 'slider-js__arrow-right');
 
-  slides.forEach((slide, index) => {
-    slide.classList.add('slider-js__item');
-    list.appendChild(slide);
-  });
+  function init() {
+    addClasses();
+    addElements();
+  }
 
-  wrapper.appendChild(list);
-  slider.appendChild(wrapper);
-  slider.appendChild(arrowLeft);
-  slider.appendChild(arrowRight);
+  function addClasses() {
+    slider.classList.add('slider-js');
+    wrapper.classList.add('slider-js-wrap');
+    list.classList.add('slider-js-list');
+    arrowLeft.classList.add('slider-js__arrow', 'slider-js__arrow-left');
+    arrowRight.classList.add('slider-js__arrow', 'slider-js__arrow-right');
+  }
+
+  function addElements() {
+    slides.forEach((slide, index) => {
+      slide.classList.add('slider-js__item');
+      list.appendChild(slide);
+    });
+  
+    wrapper.appendChild(list);
+    slider.appendChild(wrapper);
+    slider.appendChild(arrowLeft);
+    slider.appendChild(arrowRight);
+  }
+
+  init();
 }
